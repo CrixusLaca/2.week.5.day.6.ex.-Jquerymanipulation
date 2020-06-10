ef08345fa5c0e7396f06e919292541e869238949
BristyanLaszlo <bristyan.laszlo@gmail.com>
2020-06-10 21:51:43 +0200

    3

---
+++ srcipts.js
@@ -0,0 +1,14 @@
+let tombandnames = ['Angela', 'Susanna', 'Norris a Chuck', 'Lacoka'];
+
+    tombandnames.forEach((item) => {
+    console.log(item);
+    });
+$('ul').append(
+ tombandnames
+);
+
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
